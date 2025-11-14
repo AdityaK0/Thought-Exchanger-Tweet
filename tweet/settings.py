aa526be7eb80e242802ec108e18e7c8f3f84ee9f
AdityaTeam8 <adiya.chaudhary@team8network.com>
2025-11-14 17:14:03 +0530

    removed creds

--- tweet/settings.py
+++ tweet/settings.py
@@ -156,8 +156,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aditya.techseria@gmail.com'
-EMAIL_HOST_PASSWORD = 'ulxl qave jfyr fpzd'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # twillo_setup
 
